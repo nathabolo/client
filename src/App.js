3501c89ba6ce3338d5c2e1bcd61a02915483fde1
nathabolo <nathabolo@gmail.com>
2023-03-03 11:00:19 +0200

    Client commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
-import movies from './images/movies.png';
+import { getPosts } from './actions/posts';
+import useStyles from './styles';
+import movies from './images/memories.png';
 
 const App = () => {
-    return (
-       <Container maxWidth="lg">
-        <AppBar position='static' color='inherit'>
-            <Typography variant='h2' align='center'>My Movies</Typography>
-            <img src={movies} alt="movies" height="60" />
-        </AppBar>
-        <Grow in>
-            <Container>
-                <Grid container justify="space-between" alignItems="stretch" spacing={3} >
-                    <Grid item xs={12} sm={7}>
-                        <Posts />
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <Form />
-                        </Grid>
-                </Grid>
-            </Container>
-        </Grow>
-       </Container>   
-    )
-}
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Add a movie</Typography>
+        <img className={classes.image} src={movies} alt="icon" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
 
 export default App;
